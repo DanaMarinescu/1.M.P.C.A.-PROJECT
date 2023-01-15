070c9c98c894bd9d4c48e4bd4f53f274600d17c2
Catalina Luca <catalinaluca192@gmail.com>
2023-01-15 12:44:56 +0200

    added tb in IR.v

--- IR.v
+++ IR.v
@@ -15,3 +15,33 @@ begin
 opcode <= temp[5:0];
 end
 endmodule
+
+module IR_tb();
+reg clk, loadIR;
+reg [5:0] inop;
+wire [5:0] opcode;
+IR cut(
+  .clk(clk),
+  .loadIR(loadIR),
+  .inop(inop),
+  .opcode(opcode)
+);
+
+initial begin
+  clk=0;
+  loadIR=0;
+  inop=0;
+  
+  #10
+  
+  clk=1;
+  forever #30  clk = ~clk;
+  end
+initial begin
+#100
+  loadIR=0;
+  inop=6'b110;
+#100
+  loadIR=1;
+end
+endmodule
