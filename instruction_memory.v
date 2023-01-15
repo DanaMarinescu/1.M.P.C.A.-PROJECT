bfdbee01b7566f50298bbef9eabef630f430aedb
KafkaPatrik <101024171+KafkaPatrik@users.noreply.github.com>
2023-01-16 00:23:53 +0200

    Update instruction_memory.v

--- instruction_memory.v
+++ instruction_memory.v
@@ -5,18 +5,15 @@ La input primeste adresa instructiunii in memorie, si la output pune instructiun
 Ex pentru adresa 3 in decimal la output se va pune instructiunea de la locatia mem[3] 
 */
 module instruction_memory (
+  input rst,
   input [15:0] addr,
   output [15:0] instruction
 );
-reg [15:0] mem[1023:0];
-reg [9:0] i;
+  reg [15:0] mem[127:0];
 assign instruction=mem[addr];
-initial begin
-  for (i = 0; i < 1024; i = i + 1) begin
-    mem[i] = 16'h0000;
-  end
 //aici se vor scrie instructiunile
 //exemplu:
+  always @ (posedge rst)begin
   mem[0] = 16'h0001; 
   mem[1] = 16'h0100; 
   mem[2] = 16'h1590; 
@@ -24,6 +21,6 @@ initial begin
   mem[4] = 16'h3109;
   mem[5] = 16'h4400; 
   mem[6] = 16'h8020; 
-  mem[7] = 16'h1231; 
-end
+  mem[7] = 16'h1231;
+  end  
 endmodule
